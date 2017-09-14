61fbc6a311ac6efd5b5f846da53b07dfa843b4f7
JONGHO-SURFACEB\jongho <ho1234c@gmail.com>
2017-09-14 14:28:11 +0900

    Develop Auth Modal

--- src/client/Auth/AuthJoinForm.js
+++ src/client/Auth/AuthJoinForm.js
@@ -6,7 +6,10 @@ class AuthJoinForm extends Component {
       <div className="join-form">
         <input type="text" placeholder="이메일"/>
         <input type="password" placeholder="비밀번호"/>
-        <button type="button" className="join-submit-btn">확인</button>
+        <input type="password" placeholder="비밀번호 확인"/>
+        <input type="text" placeholder="이름"/>
+        <button type="button" className="join-submit-btn">가입</button>
+        <button type="button" className="facebook-join-btn">페이스북으로 가입</button>
       </div>
     );
   }
